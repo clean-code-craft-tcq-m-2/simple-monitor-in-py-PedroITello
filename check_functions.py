371e392a92f2e96fbaf8110a29c714f0d78adc71
tpe1ga <PedroIvan.TelloFlores@mx.bosch.com>
2022-11-29 00:19:41 -0600

    Modify check_functions ranges according research in articles

--- check_functions.py
+++ check_functions.py
@@ -1,6 +1,8 @@
 # Function for check temp
 def temp_is_ok(temperature):
-    if temperature < 0 or temperature > 45:
+    # Update ranges using article from research
+    # https://batteryuniversity.com/article/bu-410-charging-at-high-and-low-temperatures
+    if temperature < -20 or temperature > 50:
         print('Temperature is out of range!')
         return False
     else:
@@ -9,7 +11,9 @@ def temp_is_ok(temperature):
 
 # Function for check state of charge
 def soc_is_ok(soc):
-    if soc < 20 or soc > 80:
+    # Updated ranges using article from research
+    # https://www.sciencedirect.com/science/article/pii/S2352484719310911
+    if soc < 20 or soc > 100:
         print('State of Charge is out of range!')
         return False
     else:
@@ -18,7 +22,7 @@ def soc_is_ok(soc):
 
 # Function for check change rate
 def change_rate_is_ok(charge_rate):
-    if charge_rate > 0.8:
+    if charge_rate < 0 or charge_rate > 0.8:
         print('Charge rate is out of range!')
         return False
     else:
